80bce3995ca413ef91d1a12839151d557694c18a
MOHAMMAD <135142571+MOHREZ6@users.noreply.github.com>
2024-08-25 17:15:51 +0330

    Revert "BON-1 test"

--- pages/api/WeatherApi.js
+++ pages/api/WeatherApi.js
@@ -92,7 +92,7 @@ function WeatherApp() {
             </p>
             <div className="flex justify-between mt-4">
               <div>
-                <p className="border-b border-b-2 border-teal-500 pb-7	">
+                <p className="border-b border-b-2 border-teal-500 pb-1	">
                   Humidity
                 </p>
                 <p>{weatherData.main.humidity}%</p>
